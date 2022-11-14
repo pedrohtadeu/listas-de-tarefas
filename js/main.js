093fff920cb5de0fa7640e50352d159160771d86
pedro <pedroextone123@gmail.com>
2022-11-14 16:51:05 -0300

    adcionado projeto

---
+++ js/main.js
@@ -0,0 +1,117 @@
+
+const Main = {
+
+    tasks:[],
+
+    init: function(){
+        this.cacheSelectors()
+        this.bindEvents()
+        this.getStoraged()
+        this.builTasks()
+    },
+
+    cacheSelectors: function(){
+        this.$checkButtons = document.querySelectorAll('.check')
+        this.$inputTask = document.querySelector('#inputTask')
+        this.$list = document.querySelector('#list')
+        this.$removeButtons = document.querySelectorAll('.remove')
+    },
+
+    bindEvents: function(){
+        const self = this
+        this.$checkButtons.forEach(function(button){
+            button.onclick = self.Events.checkButton_click
+        })
+
+        this.$inputTask.onkeypress = self.Events.inputTask_keypress.bind(this)
+
+        this.$removeButtons.forEach(function(button){
+            button.onclick = self.Events.removeButton_click
+        })
+    },
+
+
+    getStoraged: function(){
+        const tasks = localStorage.getItem('tasks')
+
+        this.tasks = JSON.parse(tasks)
+    },
+
+    builTasks:function(){
+        let html = ' '
+        this.task.forEach(item => {
+            html +=  `
+            <li>
+                <div class="check"></div>
+                <label class="task">
+                    ${item.task}
+                </label>
+                <button class="remove"></button>
+            </li>
+        ` 
+        this.$list.innerHTML = html
+        this.cacheSelectors()
+        this.bindEvents()
+        });
+    },
+
+    Events:{
+        checkButton_click:function(e){
+            const li = e.target.parentElement
+            const isDone = li.classList.contains('done')
+
+            if(!isDone){
+                return li.classList.add('done')
+            } 
+            
+            li.classList.remove('done')
+         },
+
+
+         inputTask_keypress:function(e){
+            const key = e.key
+            const value = e.target.value
+
+            if(key === 'Enter'){
+                this.$list.innerHTML += `
+                    <li>
+                        <div class="check"></div>
+                        <label class="task">
+                            ${value}
+                        </label>
+                        <button class="remove"></button>
+                    </li>
+                `
+
+                e.target.value = ''
+                
+                this.cacheSelectors()
+                this.bindEvents()
+
+                const savedTask = localStorage.getItem('tasks')
+                const savedTaskobj = JSON.parse(savedTask)
+
+                const obj = [
+                    {task: value},
+                    ...savedTaskobj ,
+                ]
+
+                localStorage.setItem('tasks', JSON.stringify(obj))
+            }
+         },
+
+
+         removeButton_click: function(e){
+            let li = e.target.parentElement
+            
+            li.classList.add('removed')
+
+            setTimeout(function(){
+                li.classList.add('hidden')
+            },300)
+         }
+
+    }
+}
+
+Main.init()
